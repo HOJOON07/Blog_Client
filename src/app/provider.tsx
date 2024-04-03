e56797c545f819d7891da65b1804cf5f794c7e61
hojoon <ghwns107@naver.com>
2024-04-03 23:49:51 +0900

    first init

---
+++ src/app/provider.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { SessionProvider } from 'next-auth/react';
+import { RecoilRoot } from 'recoil';
+import { useState } from 'react';
+import { ThemeProvider } from '@/theme/theme-provider';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const NextProvider = ({ children }: Props) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: Infinity,
+          },
+        },
+      }),
+  );
+
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        {/* <SessionProvider> */}
+        {children}
+        <ReactQueryDevtools />
+        {/* </SessionProvider> */}
+      </QueryClientProvider>
+    </RecoilRoot>
+  );
+};
+
+export const NextLayout = ({ children }: Props) => {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+};
