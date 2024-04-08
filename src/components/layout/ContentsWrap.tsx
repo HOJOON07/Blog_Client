7ad7e7a99d9a0c9b24dc91febaa860e8ee7e20f2
hojoon <ghwns107@naver.com>
2024-04-09 02:17:54 +0900

    grid template error

--- src/components/layout/ContentsWrap.tsx
+++ src/components/layout/ContentsWrap.tsx
@@ -1,23 +1,10 @@
 import Nav from '../nav/Nav';
-import PostCard from '../postCard/PostCard';
 
 export default function ContentsWrap() {
   return (
     <main className="relative h-full max-h-screen transition-all duration-200 ease-in-out xl:ml-[250px] rounded-xl">
       <Nav />
-      <div className="grid grid-cols-4 gap-4 mx-6">
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-      </div>
+      <div className="grid grid-cols-4 gap-4 mx-6"></div>
     </main>
   );
 }
