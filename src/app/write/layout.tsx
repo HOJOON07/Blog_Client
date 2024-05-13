0b282f00fab334904d5cb771bfe9143fb0512f80
hojoon <ghwns107@naver.com>
2024-05-13 23:52:36 +0900

    workspace

--- src/app/write/layout.tsx
+++ src/app/write/layout.tsx
@@ -4,12 +4,6 @@ import React, { ReactNode } from 'react';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    // <div
-    //   className={cn(
-    //     'min-h-screen bg-background font-sans antialiased',
-    //     '[&_.slate-selected]:!bg-primary/20 [&_.slate-selection-area]:border [&_.slate-selection-area]:border-primary [&_.slate-selection-area]:bg-primary/10',
-    //   )}
-    // >
     <TooltipProvider
       disableHoverableContent
       delayDuration={200}
@@ -17,6 +11,5 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     >
       {children}
     </TooltipProvider>
-    // </div>
   );
 }
--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -6,8 +6,6 @@ export default function page() {
   return (
     <div className="container">
       <PostSetup />
-      {/* 포스트 헤더가 있고 */}
-
       <div className="max-w-[1400px] rounded-lg border bg-background shadow">
         <PlateEditor />
       </div>
