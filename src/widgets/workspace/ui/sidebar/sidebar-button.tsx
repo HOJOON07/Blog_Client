bb0ae9030585450e9f28993d7271fd630cd3f866
hojoon <ghwns107@naver.com>
2024-06-08 19:13:05 +0900

    디자인 시스템 입히기

--- src/widgets/workspace/ui/sidebar/sidebar-button.tsx
+++ src/widgets/workspace/ui/sidebar/sidebar-button.tsx
@@ -1,14 +1,29 @@
+'use client';
 import { Icon, IconList } from '@/shared';
+import { useRouter } from 'next/navigation';
+import path from 'path';
 
 interface SidebarButtonProps {
   icon: IconList;
   title: string;
+  pathName?: string;
 }
 
-export const SidebarButton = ({ icon, title }: SidebarButtonProps) => {
+export const SidebarButton = ({
+  icon,
+  title,
+  pathName,
+}: SidebarButtonProps) => {
+  const router = useRouter();
+  const handleRouter = () => {
+    if (pathName) router.push(pathName);
+  };
   return (
     <div className="w-full mt-auto">
-      <button className="inline-flex h-12 animate-shimmer items-center justify-start rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-1 focus:ring-slate-400 gap-8 w-full">
+      <button
+        className="inline-flex h-12 animate-shimmer items-center justify-start rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-1 focus:ring-slate-400 gap-8 w-full"
+        onClick={handleRouter}
+      >
         <Icon name={icon} />
         {title}
       </button>
--- src/widgets/workspace/ui/sidebar/sidebar-footer.tsx
+++ src/widgets/workspace/ui/sidebar/sidebar-footer.tsx
@@ -1,11 +1,11 @@
 import { SidebarButton } from '@/widgets/workspace';
+import { useRouter } from 'next/navigation';
 
 export const Footer = () => {
   return (
-    // flex items-center flex-[0_0_auto] justify-between px-3 h-[52px]
     <div className="flex flex-col gap-[6px] px-3 my-4">
-      <SidebarButton icon="plus" title="새 글 작성" />
-      <SidebarButton icon="plus" title="나가기" />
+      <SidebarButton icon="plus" title="새 글 작성" pathName="/" />
+      <SidebarButton icon="plus" title="나가기" pathName="/" />
     </div>
   );
 };
