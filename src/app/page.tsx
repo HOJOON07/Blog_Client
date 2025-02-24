d49f1cd7e26710095014011994d158b4f6d7be7d
hojoon07 <ghwns107@naver.com>
2025-02-24 23:53:14 +0900

    remove : remove typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,6 @@ export default function Page() {
           <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
           <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
           <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
-          <p className="">안녕하세요ㄴ</p>
           <Sparkles
             background="transparent"
             minSize={0.4}
